7b31c8e39531d0a97c0eec7913578636eb71a0f4
chouguting <chouguting@gmail.com>
2021-10-14 01:24:38 +0800

    Initial Commit

---
+++ pathAndShape/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  pathAndShape
+//
+//  Created by 周固廷 on 2021/10/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pathAndShape/pathAndShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  pathAndShapeApp.swift
+//  pathAndShape
+//
+//  Created by 周固廷 on 2021/10/14.
+//
+
+import SwiftUI
+
+@main
+struct pathAndShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
